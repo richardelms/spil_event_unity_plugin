fef270b488e3f110ac68640ec98e975158c00cee
Richard Elms <richardelms@gmail.com>
2015-09-18 09:45:00 +0100

    Added unity 4 version

---
+++ Unity_4_version/UNITY_4_spilSDK/Assets/Plugins/iOS/UnityAppController+PushNotificationHelper.h
@@ -0,0 +1,13 @@
+//
+//  UnityAppController+PushNotificationHelper.h
+//  Unity-iPhone
+//
+//  Created by Martijn van der Gun on 6/29/15.
+//
+//
+
+#import "UnityAppController.h"
+
+@interface UnityAppController (Delegates)
+
+@end

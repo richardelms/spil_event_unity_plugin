df7ea168503ef761eb8e62e6043e1bd356f294ed
PeeGee85Spil <R.Lubking@spilgames.com>
2016-06-20 12:11:04 +0200

    Wallet/Inventory/Shop JSON deserialisation bugfixes
    Yay!

--- PluginSource/Assets/Spilgames/Helpers/SpilGameDataHelper.cs
+++ PluginSource/Assets/Spilgames/Helpers/SpilGameDataHelper.cs
@@ -12,32 +12,23 @@ namespace SpilGames.Unity.Helpers
 	/// </summary>
 	public class SpilGameDataHelper
 	{
-		public List<Currency> Currencies;
-		public List<Item> Items;
-		public List<Bundle> Bundles;
+		public List<Currency> Currencies = new List<Currency>();
+		public List<Item> Items = new List<Item>();
+		public List<Bundle> Bundles = new List<Bundle>();
 		public Shop Shop;
-		public List<Promotion> Promotions;
+		public List<Promotion> Promotions = new List<Promotion>();
 		
 		public SpilGameDataHelper (List<SpilCurrencyData> currencies , List<SpilItemData> items, List<SpilBundleData> bundles, List<SpilShopTabData> shop, List<SpilShopPromotionData> promotions)
 		{
-			//Adding currency data to helper
-			Currencies = new List<Currency>();
-
 			foreach(SpilCurrencyData spilCurrencyData in currencies)
 			{
 				Currencies.Add(new Currency(spilCurrencyData.id, spilCurrencyData.name, spilCurrencyData.type));
 			}
-			
-			//Adding item data to helper
-			Items = new List<Item>();
 
-			foreach(SpilItemData spilCurrencyItems in items)
+            foreach (SpilItemData spilCurrencyItems in items)
 			{
 				Items.Add(new Item(spilCurrencyItems.id, spilCurrencyItems.name, spilCurrencyItems.type));
 			}
-			
-			//Adding bundle data to helper
-			Bundles = new List<Bundle>();
 
 			foreach(SpilBundleData spilBundleData in bundles)
 			{
@@ -47,14 +38,10 @@ namespace SpilGames.Unity.Helpers
 			//Adding shop data to helper
 			Shop = new Shop(shop);
 
-			//Adding bundle promotion data to helper
-			Promotions = new List<Promotion>();
-
 			foreach (SpilShopPromotionData promotion in promotions) 
 			{
 				Promotions.Add (new Promotion (promotion.bundleId, promotion.amount, promotion.prices, promotion.discount, promotion.startDate, promotion.endDate));
 			}
-
 		}
 	}
 
@@ -66,26 +53,26 @@ namespace SpilGames.Unity.Helpers
 		/// <summary>
 		/// The currency Id
 		/// </summary>
-		public int Id { get { return id; } }
-		private int id;
+		public int Id { get { return _Id; } }
+		private int _Id;
 
 		/// <summary>
 		/// The currency Name
 		/// </summary>
-		public string Name { get { return name; } }
-		private string name;
+		public string Name { get { return _Name; } }
+		private string _Name;
 
 		/// <summary>
 		/// The currency Type (Premium and Non-Premium)
 		/// </summary>
-		public int Type { get { return type; } }
-		public int type;
+		public int Type { get { return _Type; } }
+		public int _Type;
 		
 		public Currency(int id, string name, int type)
 		{
-			this.id = id;
-			this.name = name;
-			this.type = type;
+			_Id = id;
+			_Name = name;
+			_Type = type;
 		}
 	}
 
@@ -97,26 +84,26 @@ namespace SpilGames.Unity.Helpers
 		/// <summary>
 		/// The item Id
 		/// </summary>
-		public int Id { get { return id; } }
-		private int id;
+		public int Id { get { return _Id; } }
+		private int _Id;
 
 		/// <summary>
 		/// The item Name
 		/// </summary>
-		public string Name { get { return name; } }
-		private string name;
+		public string Name { get { return _Name; } }
+		private string _Name;
 
 		/// <summary>
 		/// The item Type (Consumable, Booster and Permanent)
 		/// </summary>
-		public int Type { get { return type; } }
-		public int type;
+		public int Type { get { return _Type; } }
+		public int _Type;
 		
 		public Item(int id, string name, int type)
 		{
-			this.id = id;
-			this.name = name;
-			this.type = type;
+			_Id = id;
+			_Name = name;
+			_Type = type;
 		}
 	}
 
@@ -128,45 +115,42 @@ namespace SpilGames.Unity.Helpers
 		/// <summary>
 		/// The bundle Id
 		/// </summary>
-		public int Id { get { return id; } }
-		private int id;
+		public int Id { get { return _Id; } }
+		private int _Id;
 
 		/// <summary>
 		/// The bundle Name
 		/// </summary>
-		public string Name { get { return name; } }
-		private string name;
+		public string Name { get { return _Name; } }
+		private string _Name;
 
 		/// <summary>
 		/// The bundle Prices. Can have multiple prices with different currencies
 		/// </summary>
-		public List<BundlePrice> Prices { get { return prices; } }
-		private List<BundlePrice> prices;
+		public List<BundlePrice> Prices { get { return _Prices; } }
+		private List<BundlePrice> _Prices = new List<BundlePrice>();
 
 		/// <summary>
 		/// The bundle Items that are contained within a Bundle
 		/// </summary>
-		public List<BundleItem> Items { get { return items; } }
-		private List<BundleItem> items;
+		public List<BundleItem> Items { get { return _Items; } }
+		private List<BundleItem> _Items = new List<BundleItem>();
 		
-		public Bundle(int id, string name, List<SpilBundlePriceData> prices, List<SpilBundleItemData> items){
-			this.id = id;
-			this.name = name;
-			
-			this.prices = new List<BundlePrice>();
-			
+		public Bundle(int id, string name, List<SpilBundlePriceData> prices, List<SpilBundleItemData> items)
+        {
+			_Id = id;
+			_Name = name;
+					
 			//Adding Prices for Bundle
 			foreach(SpilBundlePriceData bundlePriceData in prices)
 			{
-				this.prices.Add(new BundlePrice(bundlePriceData.currencyId, bundlePriceData.value));
+				_Prices.Add(new BundlePrice(bundlePriceData.currencyId, bundlePriceData.value));
 			}
-			
-			this.items = new List<BundleItem>();
 			
 			//Adding Items to Bundle
 			foreach(SpilBundleItemData bundleItemData in items)
 			{	
-				this.items.Add(new BundleItem(bundleItemData.id, bundleItemData.amount));
+				_Items.Add(new BundleItem(bundleItemData.id, bundleItemData.amount));
 			}
 		}
 	}
@@ -179,19 +163,19 @@ namespace SpilGames.Unity.Helpers
 		/// <summary>
 		/// The currency Id assoctiated with the Bundle Price
 		/// </summary>
-		public int CurrencyId { get { return currencyId; } }
-		private int currencyId;
+		public int CurrencyId { get { return _CurrencyId; } }
+		private int _CurrencyId;
 
 		/// <summary>
 		/// The value required in order to consume the bundle for the assoctiated currency
 		/// </summary>
-		public int Value { get { return value; } }
-		private int value;
+		public int Value { get { return _Value; } }
+		private int _Value;
 		
 		public BundlePrice(int currencyId, int value)
 		{
-			this.currencyId = currencyId;
-			this.value = value;
+			_CurrencyId = currencyId;
+			_Value = value;
 		}
 	}
 
@@ -203,19 +187,19 @@ namespace SpilGames.Unity.Helpers
 		/// <summary>
 		/// The item Id assoctiated with the Bundle Item
 		/// </summary>
-		public int Id { get { return id; } }
-		private int id;
+		public int Id { get { return _Id; } }
+		private int _Id;
 
 		/// <summary>
 		/// The amount if items contained withing the Bundle Item
 		/// </summary>
-		public int Amount { get { return amount; } }
-		private int amount;
+		public int Amount { get { return _Amount; } }
+		private int _Amount;
 		
 		public BundleItem(int id, int amount)
 		{
-			this.id = id;
-			this.amount = amount;
+			_Id = id;
+			_Amount = amount;
 		}
 	}
 
@@ -227,17 +211,14 @@ namespace SpilGames.Unity.Helpers
 		/// <summary>
 		/// The Shop Tabs as defined in SLOT
 		/// </summary>
-		public List<Tab> Tabs { get { return tabs; } }
-		private List<Tab> tabs;
+		public List<Tab> Tabs { get { return _Tabs; } }
+		private List<Tab> _Tabs = new List<Tab>();
 
 		public Shop(List<SpilShopTabData> shop)
 		{
-
-			this.tabs = new List<Tab>();
-
 			foreach (SpilShopTabData tab in shop) 
 			{
-				this.tabs.Add (new Tab(tab.name, tab.entries));
+				_Tabs.Add (new Tab(tab.name, tab.entries));
 			}
 		}
 	}
@@ -250,24 +231,22 @@ namespace SpilGames.Unity.Helpers
 		/// <summary>
 		/// The tab Name
 		/// </summary>
-		public string Name { get { return name; } }
-		private string name;
+		public string Name { get { return _Name; } }
+		private string _Name;
 
 		/// <summary>
 		/// The Shop Entries as defined in SLOT
 		/// </summary>
-		public List<Entry> Entries { get { return entries; } }
-		private List<Entry> entries;
+		public List<Entry> Entries { get { return _Entries; } }
+		private List<Entry> _Entries = new List<Entry>();
 
 		public Tab(string name, List<SpilShopEntryData> entries)
 		{
-			this.name = name;
-
-			this.entries = new List<Entry>();
+			_Name = name;
 
 			foreach (SpilShopEntryData entry in entries) 
 			{
-				this.entries.Add (new Entry(entry.bundleId, entry.label, entry.position));
+				_Entries.Add (new Entry(entry.bundleId, entry.label, entry.position));
 			}
 		}
 	}
@@ -280,26 +259,26 @@ namespace SpilGames.Unity.Helpers
 		/// <summary>
 		/// The bundle Id assoctiated with this shop entry
 		/// </summary>
-		public int BundleId { get { return bundleId; } }
-		private int bundleId;
+		public int BundleId { get { return _BundleId; } }
+		private int _BundleId;
 
 		/// <summary>
 		/// The entry label
 		/// </summary>
-		public string Label { get { return label; } }
-		private string label;
+		public string Label { get { return _Label; } }
+		private string _Label;
 
 		/// <summary>
 		/// The entry position as defined in SLOT
 		/// </summary>
-		public int Position { get { return position; } }
-		private int position;
+		public int Position { get { return _Position; } }
+		private int _Position;
 
 		public Entry(int bundleId, string label, int position)
 		{
-			this.bundleId = bundleId;
-			this.label = label;
-			this.position = position;
+			_BundleId = bundleId;
+			_Label = label;
+			_Position = position;
 		}
 	}
 
@@ -311,57 +290,53 @@ namespace SpilGames.Unity.Helpers
 		/// <summary>
 		/// The bundle Id assoctiated with this shop promotion
 		/// </summary>
-		public int BundleId { get { return bundleId; } }
-		private int bundleId;
+		public int BundleId { get { return _BundleId; } }
+		private int _BundleId;
 
 		/// <summary>
 		/// The amount of bundles for the shop promotion
 		/// </summary>
-		public int Amount { get { return amount; } }
-		private int amount;
+		public int Amount { get { return _Amount; } }
+		private int _Amount;
 
 		/// <summary>
 		/// The promotion Prices. Can have multiple prices with different currencies
 		/// </summary>
-		public List<BundlePrice> Prices { get { return prices; } }
-		private List<BundlePrice> prices;
+		public List<BundlePrice> Prices { get { return _Prices; } }
+		private List<BundlePrice> _Prices = new List<BundlePrice>();
 
 		/// <summary>
 		/// The promotion discount label
 		/// </summary>
-		public string Discount { get { return discount; } }
-		private string discount;
+		public string Discount { get { return _Discount; } }
+		private string _Discount;
 
 		/// <summary>
 		/// The begin date of an active promotion for this bundle. Returns DateTime.Min if there is no active promotion for this bundle.
 		/// </summary>
-		public DateTime PromotionBeginDate { get { return startDate; } }
-		private DateTime startDate;
+		public DateTime PromotionBeginDate { get { return _StartDate; } }
+		private DateTime _StartDate;
 
 		/// <summary>
 		/// The begin date of an active promotion for this bundle. Returns DateTime.Min if there is no active promotion for this bundle.
 		/// </summary>
-		public DateTime PromotionEndDate { get { return endDate; } }
-		private DateTime endDate;
+		public DateTime PromotionEndDate { get { return _EndDate; } }
+		private DateTime _EndDate;
 
 		public Promotion(int bundleId, int amount, List<SpilBundlePriceData> prices, string discount, DateTime startDate, DateTime endDate)
 		{
-			this.bundleId = bundleId;
-			this.amount = amount;
-
-			//Adding Prices for Bundle Promotion
-			this.prices = new List<BundlePrice>();
+			_BundleId = bundleId;
+			_Amount = amount;
 
 			foreach(SpilBundlePriceData bundlePriceData in prices)
 			{
-				this.prices.Add(new BundlePrice(bundlePriceData.currencyId, bundlePriceData.value));
+				_Prices.Add(new BundlePrice(bundlePriceData.currencyId, bundlePriceData.value));
 			}
 
-			this.discount = discount;
-			this.startDate = startDate;
-			this.endDate = endDate;
+			_Discount = discount;
+			_StartDate = startDate;
+			_EndDate = endDate;
 		}
 	}
-
 }
 

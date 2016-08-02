5a04584ccb62b25502ff1a6292aa4139d422e8fc
Richard Elms <richardelms@gmail.com>
2016-08-02 12:57:30 +0200

    spil editor window now working correctly, ready for more testing

--- PluginSource/Assets/Editor/SpilEditor.cs
+++ PluginSource/Assets/Editor/SpilEditor.cs
@@ -2,11 +2,9 @@
 using System.Collections;
 using UnityEditor;
 using System.IO;
-
+using SpilGames.Unity.Utils;
 public class SpilEditor : EditorWindow 
 {
-
-	public string bundleID;
 
 	[MenuItem ("Window/Spilgames", false, 1)]
 	static void Init () {
@@ -21,8 +19,7 @@ public class SpilEditor : EditorWindow
 	void OnGUI()
 	{
 		GUILayout.Label( "SpilSDK Config Import", EditorStyles.boldLabel );
-		GUILayout.Label ("Bundle ID", EditorStyles.label);
-		bundleID = GUILayout.TextField (bundleID);
+		GUILayout.Label ("WARNING: Please make sure to set your bundle ID in your player settings before getting your game data.\nPlease also note that this tool will only work if your build target is set to iOS or Android");
 		if (GUILayout.Button ("Create Default Configs")) {
 			CreateDefaultConfigFiles ();
 		}
@@ -30,16 +27,59 @@ public class SpilEditor : EditorWindow
 
 
 	void CreateDefaultConfigFiles(){
-		if(bundleID == ""){
+		if(PlayerSettings.bundleIdentifier == ""){
 			throw new UnityException ("Bundle ID is Blank");
 		}
-		Debug.Log ("Getting Default Game Data for: " + bundleID);
+		Debug.Log ("Getting Default Game Data for: " + PlayerSettings.bundleIdentifier);
 		string streamingAssetsPath = Application.dataPath + "/StreamingAssets";
 		Directory.CreateDirectory(streamingAssetsPath);
-		File.Create (streamingAssetsPath + "/defaultGameConfig.json");
-		File.Create (streamingAssetsPath + "/defaultGameData.json");
-		File.Create (streamingAssetsPath + "/defaultPlayerData.json");
+		File.WriteAllText (streamingAssetsPath + "/defaultGamedata.json", GetData ("requestGameData"));
+		File.WriteAllText (streamingAssetsPath + "/defaultGameConfig.json", GetData ("requestConfig"));
+		File.WriteAllText (streamingAssetsPath + "/defaultPlayerData.json", GetData ("requestPlayerData"));
 	}
 
+	string GetData(string type){
+		string gameData = "";
+		WWWForm form = GetFormData ();
+		form.AddField ("name", type);
+		WWW request = new WWW ("https://apptracker.spilgames.com/android_event", form);
+		while(!request.isDone);
+		if (request.error != null) {
+			Debug.LogError ("Error getting game data: " + request.error);  
+		} else { 
+			Debug.Log (type + " Data returned: " + request.text);
+			JSONObject serverResponce = new JSONObject (request.text);
+			gameData = serverResponce.GetField ("data").ToString ();
+		}
+		return gameData;
+	}
+
+	WWWForm GetFormData(){
+		JSONObject dummyData = new JSONObject ();
+		dummyData.AddField ("uid","deadbeef");
+		dummyData.AddField ("locale","en");
+		dummyData.AddField ("appVersion","1");
+		dummyData.AddField ("apiVersion","1");
+		dummyData.AddField ("os","Android");
+		dummyData.AddField ("osVersion","1");
+		dummyData.AddField ("deviceModel","Backend");
+		dummyData.AddField ("packageName",PlayerSettings.bundleIdentifier);
+		dummyData.AddField ("tto","0");
+		dummyData.AddField ("sessionId","deadbeef");
+		dummyData.AddField ("timezoneOffset","0");
+		JSONObject dummyCustomData = new JSONObject ();
+		JSONObject dummyWallet = new JSONObject ();
+		dummyWallet.AddField ("offset", 0);
+		JSONObject dummyInventory = new JSONObject ();
+		dummyInventory.AddField ("offset", 0);
+		dummyCustomData.AddField ("wallet",dummyWallet);
+		dummyCustomData.AddField ("inventory",dummyInventory);
+		WWWForm form = new WWWForm ();
+		form.AddField ("data",dummyData.ToString());
+		form.AddField ("customData",dummyCustomData.ToString());
+		form.AddField ("ts", "1470057439857");
+		form.AddField ("queued", "0");
+		return form;
+	}
 
 }
